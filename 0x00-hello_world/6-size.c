0bc238b4d553ef571c79f9ed615e95a555d90598
kilemile26 <harunakilemile@gmail.com>
2023-03-20 16:14:19 -0700

    type size1

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,10 +7,10 @@
 int main(void)
 
 {
-	print("size of char: %d byte(s)\n", sizeof(char));
-	print("size of int: %d byte(s)\n", sizeof(int));
-	print("size of long int: %d byte(s)\n", sizeof(long int));
-	print("size of long long int: %d byte(s)\n", sizeof(long long int));
-	print("size of float: %d byte(s)", sizeof(float));
+	printf("size of char: %lu byte(s)\n", sizeof(char));
+	printf("size of int: %lu byte(s)\n", sizeof(int));
+	printf("size of long int: %lu byte(s)\n", sizeof(long int));
+	printf("size of long long int: %lu byte(s)\n", sizeof(long long int));
+	printf("size of float: %lu byte(s)", sizeof(float));
 	return (0);
 }
