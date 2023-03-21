ddf496180549635c762742689923dd3076db712b
kilemile26 <harunakilemile@gmail.com>
2023-03-21 16:23:07 -0700

    checking for lower case

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *_islower - shows if the character is lower case
+ *@c: parameter to be printed
+ *Return: 1 if it is lower case and 0 otherwise
+ */
+
+int _islower(int c);
+{
+	if (c <= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n')
+}
