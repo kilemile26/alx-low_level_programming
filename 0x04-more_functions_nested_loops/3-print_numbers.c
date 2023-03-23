f8ce2334a57b9a5adace3a53612dd9cdb43fb339
kilemile26 <harunakilemile@gmail.com>
2023-03-23 14:57:39 -0700

    function that prints the numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - printing function
+ *
+ * Return: 0
+ */
+
+void print_numbers(void)
+{
+	char i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
