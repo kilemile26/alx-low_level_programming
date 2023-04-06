d10392067856ec2f6c075a827a67d43d1ca569bd
kilemile26 <harunakilemile@gmail.com>
2023-04-06 10:10:23 -0700

    puts using recursion1.

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,16 +1,18 @@
 #include "main.h"
 
 /**
- * main - check the code
- * @s: a pointer used to point 
- * Return: void
+ * _puts_recursion - puts function using recursion
+ * @s: a pointer used to point
+ * * Return: void
  */
 
 void _puts_recursion(char *s)
 {
 	if (*s == '\0')
+	{
 		_putchar('\n');
 	return;
+	}
 	_putchar(*s);
 	_puts_recursion(s + 1);
 }
