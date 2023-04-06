fa2ff49b3075afd5132a539575691881aa424028
kilemile26 <harunakilemile@gmail.com>
2023-04-06 09:38:07 -0700

    count and rev string1

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -2,12 +2,13 @@
 
 /**
  * _strlen_recursion - string counting function
+ * @s: pointer pointing to string char
  * Return: Always 0 if count finishes.
  */
 
 int _strlen_recursion(char *s)
-{	
-	if(*s == '\0')
-        return 0;
-	return 1 + _strlen_recursion(s + 1);
+{
+	if (*s == '\0')
+		return (0);
+	return (1 + _strlen_recursion(s + 1));
 }
