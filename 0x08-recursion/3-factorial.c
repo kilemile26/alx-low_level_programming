ecec36392a939d875de847c829b32169e0141893
kilemile26 <harunakilemile@gmail.com>
2023-04-06 07:07:19 -0700

    factorial correction

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,17 +1,16 @@
 #include "main.h"
 
 /**
- * factorial - funtion for factorial calculation 
- *
+ * factorial - funtion for factorial calculation
  * @n: number to find it's factorial
- * Return: 1 for last factorial, -1 for error if n < 0 and 0 at the end of program
+ * Return: -1 for error otherwise 0
  */
 
 int factorial(int n)
 {
-	if (n<=1)
-		return 1;
-	else if (n<0){}
-	return -1;
-	return n * factorial(n - 1);
+	if (n <= 1)
+		return (1);
+	else if (n < 0)
+	return (-1);
+	return (n * factorial(n - 1));
 }
