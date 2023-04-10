3e65ad0d80d88c9a9abda8b4b99f1862ec90da08
kilemile26 <harunakilemile@gmail.com>
2023-04-10 06:28:37 -0700

    locate a char

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,15 +1,21 @@
 #include "main.h"
 
 /**
- * 2-strchr.c
- *
+ * *_strchr - function that finds first occurence of c
+ * @s: pointer used to point at the character
+ * @c: the character to be found
  * Return: a pointer to the first occurrence of the character
  */
 
-char *_strchr(char *s, char c);
+char *_strchr(char *s, char c)
 {
-	char *s;
-	char c;
-	
-	
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+			s++;
+		}
+	}
+		return (s);
 }
