e5541c5d68213ee1e83bcbd24be4272954641bb3
kilemile26 <harunakilemile@gmail.com>
2023-04-12 08:43:06 -0700

    create an array1

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,20 +13,19 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 	char *arr;
 
-	if(size == 0){
-		return ('\0');
-	}
-	arr = (char*) malloc(size * sizeof(char));
-	if(arr == NULL)
+	if (size == 0)
+		return (NULL);
+
+	arr = (char *) malloc(size * sizeof(char));
+	if (arr == NULL)
 	{
-		return NULL;
-		exit(1);
+		return (NULL);
 	}
-	
+
 	for (i = 0; i < size; i++)
 	{
 		arr[i] = c;
 	}
 	free(arr);
-	return(arr);
+	return (arr);
 }
