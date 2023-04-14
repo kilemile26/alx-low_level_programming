e3d9f74c4f0053695f44834629759a59d5e785a5
kilemile26 <harunakilemile@gmail.com>
2023-04-14 08:53:03 -0700

    tasks 0&1

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * *string_nconcat - concatenates n bytes of a string to another string
+ * @s1: string to append to
+ * @s2: string to concatenate from
+ * @n: number of bytes from s2 to concatenate to s1
+ * Return: pointer to the resulting string		
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *s0;
+	unsigned int a, b;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	a = strlen(s1);
+	b = strlen(s2);
+	if (n >= b)
+		n = b;
+	s0 = malloc((a + n + 1) * sizeof(char));
+	if (s0 == NULL)
+		return NULL;
+	strcpy(s0, s1);
+	strncat(s0, s2, n);
+	s0[a + n] = '\0';
+	
+	return s0;
+}
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,6 @@
+0x0C. C - More malloc, free
+
+1. 0. Trust no one
+2. 1. string_nconcat
+3. 2. _calloc
+4. 3. array_range
