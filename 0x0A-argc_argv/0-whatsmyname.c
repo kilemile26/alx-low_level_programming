cea37e65c5bc7d4e077f6b6df354d7376bb6d2ac
kilemile26 <harunakilemile@gmail.com>
2023-04-15 11:56:41 -0700

    my name is

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - printing the argument name only
+ * @argc: integer variables
+ * @argv: array of pointers to arguments
+ * return: 0
+ */
+
+int main(int argc, char **argv)
+{
+	(void) argc;
+	printf("%s\n", *argv);
+	return (0);
+}
