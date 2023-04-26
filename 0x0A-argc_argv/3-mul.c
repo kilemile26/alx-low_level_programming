44725b5f9c0273e14199a86e00045a774d52ac46
kilemile26 <harunakilemile@gmail.com>
2023-04-26 09:00:59 -0700

    mul of args2

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -4,26 +4,18 @@
  * main - printing names of the program
  * @argc: number of arguments
  * @argv: array of pointers to arguments
- * @u: argument 1
- * @i: argument 2
- * @result: answer to mul
  * Return: Always 0.
  */
 int main(int argc, char *argv[]__attribute__((unused)))
 {
-	int i;
-	int result;
-	int arg1;
-	int arg2;
+	int result, arg1, arg2;
 
-	for (i = 0; i < argc; i++)
+	if (argc != 3)
 	{
-		if (argc != 3)
-		{
-			printf("Error\n");
-			return (1);
-		}
+		printf("Error\n");
+		return (1);
 	}
+
 	arg1 = atoi(argv[1]);
 	arg2 = atoi(argv[2]);
 	result = arg1 * arg2;
