6f459a52e9201b986ab652fba41ffe8dbd926a1a
kilemile26 <harunakilemile@gmail.com>
2023-04-26 10:57:44 -0700

    To infinity and beyond1

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -12,26 +12,30 @@
 int main(int argc, char *argv[])
 {
 	int sum = 0;
-	int i;
+	int i, j, num;
 
-    for (i = 1; i < argc; i++) {
-        const char *arg = argv[i];
-        int len = strlen(arg);
+	for (i = 1; i < argc; i++)
+	{
+		const char *arg = argv[i];
+		int len = strlen(arg);
 
-        for (int j = 0; j < len; j++) {
-            if (!isdigit(arg[j])) {
-                printf("Error\n");
-                return 1;
-            }
-        }
+		for (j = 0; j < len; j++)
+		{
+			if (!isdigit(arg[j]))
+			{
+				printf("Error\n");
+				return 1;
+			}
+		}
 
-        int num = atoi(arg);
-        if (num > 0) {
-            sum += num;
-        }
-    }
+		num = atoi(arg);
+		if (num > 0) 
+		{
+			sum += num;
+		}
+	}
 
-    printf("%d\n", sum);
+	printf("%d\n", sum);
 
-    return (0);
+	return (0);
 }
