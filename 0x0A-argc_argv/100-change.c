ce38027730cbef3082fe3a48b9fdbcb1862ac5dd
kilemile26 <harunakilemile@gmail.com>
2023-04-26 10:28:00 -0700

    change coins1

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,6 +1,12 @@
 #include "main.h"
 
-
+/*
+ * main - Entry point for the program that prints minimum number of coins
+ *
+ * @argc: number of args
+ * @argv: pointer to args
+ * Return: 0
+ */
 
 int main(int argc, char *argv[])
 {
@@ -10,25 +16,25 @@ int main(int argc, char *argv[])
 	if (argc != 2)
 	{
 		printf("Error\n");
-		return 1;
+		return (1);
 	}
 	amount = atoi(argv[1]);
 
 	if (amount < 0)
 	{
 		printf("0\n");
-		return 0;
+		return (0);
 	}
 
 	num_coins = 0;
 
-	for (i = 0; i < 5 && amount > 0; i++) /*loop through each denomination*/
+	for (i = 0; i < 5 && amount > 0; i++)
 	{
-		num_coins += amount / coins[i]; /* count the number of coins required for this denomination*/
-		amount %= coins[i]; /*calculate the remaining amount after using this denomination*/
+		num_coins += amount / coins[i];
+		amount %= coins[i];
 	}
 
 	printf("%d\n", num_coins);
 
-	return 0;
+	return (0);
 }
