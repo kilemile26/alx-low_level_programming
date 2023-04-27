6aaab52d1798ebe6e507c33d119bd65a624c1737
kilemile26 <harunakilemile@gmail.com>
2023-04-27 12:24:46 -0700

    print before main1

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,11 +1,12 @@
 #include "lists.h"
 
+void before_main() __attribute__ ((constructor));
 /**
- * before_main - prints a sentence before the main
+ * void - prints a sentence before the main
  */
 
-void __attribute__ ((constructor)) before_main()
+void before_main()
 {
-	printf("You're beat! and yet, you must allow,\n
-			I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
