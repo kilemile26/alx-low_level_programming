9d045845882ace2c49110259b79a5b9758a1763c
kilemile26 <harunakilemile@gmail.com>
2023-05-02 11:26:00 -0700

    number of elements2

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,10 +1,10 @@
 #include "lists.h"
 
 /**
- * print_listint - Prints all the elements of a listint_t list.
+ *  listint_len - returns the number of elements in a linked listint_t list.
  * @h: A pointer to the head of the listint_t list.
  *
- * Return: The number of nodes in the listint_t list.
+ * Return: The number of elements in the listint_t list.
  */
 size_t listint_len(const listint_t *h)
 {
