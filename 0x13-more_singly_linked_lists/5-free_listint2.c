50d1ea9b17955a244bd9dc3a32276077c0791c4a
kilemile26 <harunakilemile@gmail.com>
2023-05-02 13:33:37 -0700

    free a list1 and set (h=0)2

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,12 +1,13 @@
 #include "lists.h"
 
 /**
- * free_listint - function that frees a listint_t list
- * @head: head of list to be freed
+ * free_listint2 - function that frees a listint_t list
+ * @head: pointer to head of list to be freed
  */
 void free_listint2(listint_t **head)
 {
 	listint_t *list;
+
 	if (head == NULL)
 		return;
 
