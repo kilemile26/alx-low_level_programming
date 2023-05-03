483ecae85658c8a25934f9b60db5dc3aeef909ec
kilemile26 <harunakilemile@gmail.com>
2023-05-03 12:12:14 -0700

    last1

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -21,18 +21,14 @@ void print_all(const char * const format, ...)
 				case 'i':
 					printf("%s%d", sep, va_arg(list, int));
 					break;
-
 				case 'c':
 					printf("%s%c", sep, va_arg(list, int));
 					break;
-
 				case 'f':
 					printf("%s%f", sep, va_arg(list, double));
 					break;
-
 				case 's':
 					str = va_arg(list, char *);
-
 					if (!str)
 					{
 						str = "(nil)";
@@ -47,7 +43,6 @@ void print_all(const char * const format, ...)
 			i++;
 		}
 	}
-
 	printf("\n");
 	va_end(list);
 }
