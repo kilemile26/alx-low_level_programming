24fd19d58e6f5736861d61e453dcdbfbaeca1a00
kilemile26 <harunakilemile@gmail.com>
2023-05-03 08:14:40 -0700

    sum of parameters1

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -15,17 +15,17 @@ int sum_them_all(const unsigned int n, ...)
 
 	if (n == 0)
 	{
-		return 0;
+		return (0);
 	}
 
 	va_start(args, n);
 
-	for (i = 0; i < n; i++) 
+	for (i = 0; i < n; i++)
 	{
 		sum += va_arg(args, int);
 	}
 
 	va_end(args);
 
-	return sum;
+	return (sum);
 }
