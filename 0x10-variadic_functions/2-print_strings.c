20399be740b38255f55fafe604ede8d32a070115
kilemile26 <harunakilemile@gmail.com>
2023-05-03 10:00:12 -0700

    print strings in line6

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -17,16 +17,19 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		char *str = va_arg(args, char *);
+		if (str == NULL)
+		{
+			printf("(nil)");
+		}
+		else
+		{
+			printf("%s", str);
+		}
 
-		printf("%s", str);
 		if (i < n - 1)
 		{
 			printf("%s", separator);
 		}
-	}
-	if (str == NULL)
-	{
-		printf("(nil)");
 	}
 	va_end(args);
 	printf("\n");
