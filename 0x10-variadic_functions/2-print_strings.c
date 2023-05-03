6ca3536918b411a0f672b0454d1c67d502741fe1
kilemile26 <harunakilemile@gmail.com>
2023-05-03 09:56:02 -0700

    print strings in line5

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -24,6 +24,10 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			printf("%s", separator);
 		}
 	}
+	if (str == NULL)
+	{
+		printf("(nil)");
+	}
 	va_end(args);
 	printf("\n");
 }
