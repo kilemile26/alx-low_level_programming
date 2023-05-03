caed123865a3644e1546deaff6a4c35be47baa4d
kilemile26 <harunakilemile@gmail.com>
2023-05-03 06:11:11 -0700

    nth node index1

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -11,7 +11,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int i = 0;
 
-	while (head != NULL && count < index)
+	while (head != NULL && i < index)
 	{
 		head = head->next;
 		i++
