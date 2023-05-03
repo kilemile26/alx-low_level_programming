d7d94e3a6b9daebd4840af0d74c3827bc1a5e899
kilemile26 <harunakilemile@gmail.com>
2023-05-03 13:06:58 -0700

    search1

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,10 @@
+#include "function_pointers.h"
+
+/**
+ * main - prints its own function's opcodes
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0
+ */
+
--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -14,7 +14,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 
 	if (array == NULL || size <= 0 || cmp == NULL)
 	{
-		return -1;
+		return (-1);
 	}
 
 	while (i < size && !cmp(array[i]))
@@ -22,5 +22,5 @@ int int_index(int *array, int size, int (*cmp)(int))
 		i++;
 	}
 
-    return (i == size ? -1 : i);
+	return (i == size ? -1 : i);
 }
