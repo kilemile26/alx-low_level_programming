640f4395d46680f8ab764c5332250c42f41cfbb7
kilemile26 <harunakilemile@gmail.com>
2023-05-03 14:37:48 -0700

    calloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * *_memset - fills memory with a constant byte
+ * @s: memory to be filled
+ * @b: character to copy
+ * @n: number of times to copy b
+ * Return: pointer to the memory area s
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	if (nmemb == 0 || size == 0)
+	{
+		return NULL;
+	}
+
+	void *ptr = malloc(nmemb * size);
+	if (ptr == NULL)
+	{
+		return NULL;
+	}
+
+	ptr = calloc(nmemb, size);
+	return ptr;
+}
+
