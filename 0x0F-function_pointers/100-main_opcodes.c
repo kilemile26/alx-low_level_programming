8c53f49d75cad7d44babc9043e057f3f020556fa
kilemile26 <harunakilemile@gmail.com>
2023-05-03 13:23:03 -0700

    print opcodes

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -8,3 +8,24 @@
  * Return: 0
  */
 
+int main(int argc, char *argv[])
+{
+	char *main_func;
+	int num_bytes;
+
+	if (argc != 2 || atoi(argv[1]) < 0)
+	{
+		printf("Error\n");
+		return (argc != 2 ? 1 : 2);
+	}
+
+	main_func = (char *)main;
+	num_bytes = atoi(argv[1]);
+
+	for (int i = 0; i < num_bytes; i++)
+	{
+		printf("%02hhx%c", main_func[i], i == num_bytes - 1 ? '\n' : ' ');
+	}
+
+	return (0);
+}
