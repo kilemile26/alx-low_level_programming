9a500e81d08c863f08e69acb39a65172f8ad022b
kilemile26 <harunakilemile@gmail.com>
2023-05-03 14:54:28 -0700

    allahu akbar

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -11,12 +11,18 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	if (nmemb == 0 || size == 0)
+	{
 		return NULL;
+	}
 
 	void *ptr = malloc(nmemb * size);
-	if (ptr != NULL)
-		memset(ptr, 0, nmemb * size);
+
+	if (ptr == NULL)
+	{
+		return NULL;
+	}
+
+	memset(ptr, 0, nmemb * size);
 
 	return ptr;
 }
-
