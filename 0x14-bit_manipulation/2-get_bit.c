5a16683ca0c58fadfaa4a2cc9fa003a9e7b3b663
kilemile26 <harunakilemile@example.com>
2023-07-10 10:50:34 -0700

    getting it2

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -14,7 +14,5 @@ int get_bit(unsigned long int n, unsigned int index)
 		return (-1);
 	}
 
-	unsigned long int bitmask = 1 << index;
-	return (n & bitmask) ? 1 : 0;
-	/*return ((n >> index) & 1);*/
+	return ((n >> index) & 1);
 }
